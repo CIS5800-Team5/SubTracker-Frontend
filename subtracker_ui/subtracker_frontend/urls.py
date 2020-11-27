b61d76b92301f1985531206256f7d54517d7e928
Jason Semon <jason.semon@nytimes.com>
2020-11-27 17:05:28 -0500

    removed ms urls

--- subtracker_ui/subtracker_frontend/urls.py
+++ subtracker_ui/subtracker_frontend/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('frontend.urls')),
-    path('user-auth/', admin.site.urls),
-    path('auth/', include('microsoft_auth.urls', namespace='microsoft')),
+    path('admin/', admin.site.urls),
 ]
