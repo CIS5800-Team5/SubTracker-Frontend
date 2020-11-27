d9596777bafc764f8105356e147dcc59168afbb1
Jason Semon <jason.semon@nytimes.com>
2020-11-27 18:37:01 -0500

    added links and fixed static files

--- subtracker_ui/subtracker_frontend/settings.py
+++ subtracker_ui/subtracker_frontend/settings.py
@@ -173,10 +173,11 @@ USE_TZ = True
 #STATICFILES_LOCATION = "static"
 #STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static")
+#]
 #STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = '/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
